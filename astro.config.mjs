335a8551db5c2c7a32023035f0b0cc3d5172dc33
Mike Carlo <mike@powerbi.tips>
2025-04-19 13:32:14 -0500

    fix: Add index file under pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
   integrations: [mdx(), sitemap(), tailwind()],
-  // redirects: {
-  //   '/': '/introduction/welcome'
-  // },
-  // Use different site URL based on environment
   site: process.env.NODE_ENV === 'production' 
     ? 'https://carloconsultants.github.io'
     : 'http://localhost:4321'
