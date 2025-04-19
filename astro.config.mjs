f26cdbab1a991ade8d939335dc02dba508be24a8
Mike Carlo <mike@powerbi.tips>
2025-04-19 12:43:45 -0500

    fix: local run and add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,23 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+const SERVER_PORT = 4321;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+
+const LIVE_URL = 'https://carloconsultants.github.io';
+
+const SCRIPT = process.env.npm_lifecycle_event || '';
+const isBuild = SCRIPT.includes('astro build');
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
   integrations: [mdx(), sitemap(), tailwind()],
-  site: process.env.ASTRO_SITE_URL || 'https://astronaut.github.io',
-  base: process.env.ASTRO_BASE || 'entelexos-docs',
   redirects: {
     '/': '/introduction/welcome'
   }
