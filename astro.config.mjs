feaf4410c74e9d3f80a5054eca3d540454c5c1a0
Mike Carlo <mike@powerbi.tips>
2025-04-19 12:57:58 -0500

    fix: add site and base for

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-const SERVER_PORT = 4321;
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-
-const LIVE_URL = 'https://carloconsultants.github.io';
-
-const SCRIPT = process.env.npm_lifecycle_event || '';
-const isBuild = SCRIPT.includes('astro build');
-let BASE_URL = LOCALHOST_URL;
-
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
-
 export default defineConfig({
-  // server: { port: SERVER_PORT },
-  site: BASE_URL,
-  base: 'entelexos-docs',
   integrations: [mdx(), sitemap(), tailwind()],
   redirects: {
     '/': '/introduction/welcome'
-  }
+  },
+  site: 'https://carloconsultants.github.io',
+  base: 'entelexos-docs'
 }); 
